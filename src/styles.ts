a30388fd02b68ff56f1ffa8b3174868dba3ed2a1
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-10-23 13:16:07 -0500

    fix: :white_check_mark: adding extra joinStyles() test coverage

--- src/styles.ts
+++ src/styles.ts
@@ -1,8 +1,9 @@
 export function joinStyles(...values: string[]) {
-	return values
-        .map(value => value.split(';').map(v => v.trim()))
-        .flat()
-        .filter(Boolean)
-        .join(';')
-        || undefined
+  return (
+    values
+      .map(value => value.split(';').map(v => v.trim()))
+      .flat()
+      .filter(Boolean)
+      .join(';') || undefined
+  )
 }
--- tests/styles.ts
+++ tests/styles.ts
@@ -3,9 +3,26 @@ import * as assert from 'uvu/assert'
 import { joinStyles } from '../src/styles.js'
 
 test('joinStyles', () => {
-    assert.type(joinStyles, 'function', 'joinStyles is a function')
-    assert.is(joinStyles(), undefined, 'undefined when no styles provided')
-    assert.is(joinStyles('color: red'), 'color: red', 'single style unchanged')
-    assert.is(joinStyles('color: red', 'background: blue'), 'color: red;background: blue', 'joins two styles')
-    assert.is(joinStyles('color: red;background: blue;', 'font-size: 16px'), 'color: red;background: blue;font-size: 16px', 'handles compound styles')
+  assert.type(joinStyles, 'function', 'joinStyles is a function')
+  assert.is(joinStyles(), undefined, 'undefined when no styles provided')
+  assert.is(joinStyles('color: red'), 'color: red', 'single style unchanged')
+  assert.is(
+    joinStyles('color: red', 'background: blue'),
+    'color: red;background: blue',
+    'joins two styles'
+  )
+  assert.is(
+    joinStyles('color: red;background: blue;', 'font-size: 16px'),
+    'color: red;background: blue;font-size: 16px',
+    'handles compound styles'
+  )
+  assert.is(
+    joinStyles(
+      'color: red; background: blue',
+      'font-size: 16px;',
+      'line-height: 1.5; font-weight: bold'
+    ),
+    'color: red;background: blue;font-size: 16px;line-height: 1.5;font-weight: bold',
+    'three complex styles'
+  )
 })
